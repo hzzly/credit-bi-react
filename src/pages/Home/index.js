9f045ddaafa1cc30748dafe18147d15cbd14a25f
黄敬仁 <hjingren@aliyun.com>
2019-08-19 15:28:17 +0800

    feat: 采用grid布局切割

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,19 @@
 import React, { PureComponent } from 'react';
+import ReactParticleLine from 'react-particle-line';
 import styles from './index.scss';
 
 export default class index extends PureComponent {
   render() {
-    return <div className={styles.homeBox}>webpack-template</div>;
+    return (
+      <ReactParticleLine>
+        <div className={styles.homeBox}>
+          <div className={styles.topLeft}></div>
+          <div className={styles.topCenter}></div>
+          <div className={styles.topRight}></div>
+          <div className={styles.bottomLeft}></div>
+          <div className={styles.bottomRight}></div>
+        </div>
+      </ReactParticleLine>
+    );
   }
 }
